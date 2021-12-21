6380123dda38273551e8ade151f6187b538eb464
Aaron Klump <sourcecode@intheloftstudios.com>
2021-12-20 20:08:17 -0800

    require php >= 7.3; update composer dependencies

---
+++ .web_package/hooks/build/10_scss.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+bump_sass=$(type sass >/dev/null &2>&1 && which sass)
+
+$bump_sass  --style=compressed --no-cache --update "$7/scss/visual_sitemap.scss:$7/dist/visual_sitemap.css"
